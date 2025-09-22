c1ae35c99c4b02b58220566df6bf994ffd90cefa
hefow <axmedaadan@gmail.com>
2025-09-22 17:47:00 +0300

    search & filtering

--- src/controller/booksController.js
+++ src/controller/booksController.js
@@ -2,8 +2,8 @@ import Book from "../model/book.js";
 
 
 export const createBook =async(req,res)=>{
-    const {title,author, price,stock}=req.body
-    if(!title || price==null) return res.status(422).json({message: "missing fields"});
+    const {title,author, price,stock}=req.body;
+    if(!title || !price==null) return res.status(422).json({message: "missing fields"});
 
     try {
         const book = await Book.create({title,author,price,stock:stock | 0});
@@ -13,7 +13,40 @@ export const createBook =async(req,res)=>{
     }
 };
 
+export const getBooks =async (req,res) =>{
+    try {
+        const {page=1,limit=10,search="",author,minPrice,maxPrice}=req.query;
+        const offset = (page - 1) * limit;
 
+        const where = {};
+
+        if(search){
+            where.title ={[Op.iLike]:`&${search}&`}
+        }
+        if(author){
+            where.author = {[Op.iLike]: `&${author}&`};
+        }
+
+        if(minPrice || maxPrice){
+            where.price ={};
+            if(minPrice) where.price[Op.gte] = parseFloat(minPrice);
+            if(maxPrice) where.price[Op.lte]= parseFloat(maxPrice);
+        }
+
+        const {count, rows} = await Book.findAndCountAll({where,limit: parseInt(limit),offset, order:[["createdAt","DESC"]]});
+
+        res.status(200).json({
+            totalBooks: count,
+            totalPages: Math.ceil(count / limit),
+            currentPage: parseInt(page),
+            books: rows,
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Server error"});
+    }
+}
 
 export const getBookById = async(req,res)=>{
     try {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,9 +1,11 @@
-import { createBook, deleteBook, getBookById, updateBook } from "../controller/booksController.js"
+import { createBook, deleteBook, getBookById, getBooks, updateBook } from "../controller/booksController.js"
 import express from 'express'
+import { isAdmin, protect } from "../middleware/auth.js"
 
 const router = express.Router()
  
-router.post("/create",createBook),
+router.post("/create",protect,isAdmin,createBook),
+router.get("/all",getBooks);
 router.get("/:id",getBookById),
 router.put("/:id",updateBook),
 router.delete("/:id",deleteBook)
